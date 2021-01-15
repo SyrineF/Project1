73aca55e61eebda5d1127ae5bb1c18337ffc5d2e
narutomk <khaled.bouflija@gmail.com>
2021-01-15 16:55:09 +0100

    <<<<<<<<<<<

--- src/components/ToDo/SingleToDo.js
+++ src/components/ToDo/SingleToDo.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./styles/ToDoList.css";
 
 function SingleToDo(props) {
@@ -13,11 +13,10 @@ function SingleToDo(props) {
       <div className="row background justify-content-between">
         <p>id: {id}</p>
         <h4>{task}</h4>
-        <button className="btn btn-warning"  onClick={detailshandler}>
-            Détails
-           
-          </button>
-     
+        <button className="btn btn-warning" onClick={detailshandler}>
+          Détails
+        </button>
+
         <button
           className="btn btn-danger"
           onClick={(e) => {
@@ -28,13 +27,17 @@ function SingleToDo(props) {
           delete Task
         </button>
       </div>
-      { details &&
+      {details && (
         <div className="container">
-          <h3>task: {task}</h3>
-          <h3>from: {from}</h3>
-          <h3>to : {to}</h3>
-          </div>
-        }
+          <form className="row justify-content-between">
+            <input type="text" value={id} />
+            <input type="text" value={task} />
+            <input type="text" value={from} />
+            <input type="text" value={to} />
+            <button className="btn btn-outline-primary">Update</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
