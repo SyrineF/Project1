5272e5d7284a4156cbdf952be3d7783f91cc906e
SyrineF <syrineferjani.isam@gmail.com>
2021-01-15 16:43:49 +0100

    details

---
+++ src/components/ToDo/Details.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Details() {
+    const { id, task, from, to } = props.data;
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Details
--- src/components/ToDo/SingleToDo.js
+++ src/components/ToDo/SingleToDo.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles/ToDoList.css";
 
 function SingleToDo(props) {
-  const { id, task } = props.data;
+  const [details, setDetails] = useState(false);
+  const { id, task, from, to } = props.data;
+
+  const detailshandler = () => {
+    setDetails(!details);
+  };
   return (
     <div className="container">
       <div className="row background justify-content-between">
         <p>id: {id}</p>
         <h4>{task}</h4>
-        <button className="btn btn-warning">Détails</button>
+        <button className="btn btn-warning"  onClick={detailshandler}>
+            Détails
+           
+          </button>
+     
         <button
           className="btn btn-danger"
           onClick={(e) => {
@@ -19,6 +28,13 @@ function SingleToDo(props) {
           delete Task
         </button>
       </div>
+      { details &&
+        <div className="container">
+          <h3>task: {task}</h3>
+          <h3>from: {from}</h3>
+          <h3>to : {to}</h3>
+          </div>
+        }
     </div>
   );
 }
