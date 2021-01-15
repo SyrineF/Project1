32bf1163284f062a0d025851535df418b96ed0e1
narutomk <khaled.bouflija@gmail.com>
2021-01-15 13:28:58 +0100

    <<<<<<<<<<<

--- src/components/ToDo/ToDoList.js
+++ src/components/ToDo/ToDoList.js
@@ -1,7 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import SingleToDo from "./SingleToDo";
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
 
 function ToDoList() {
   const todoList = [
@@ -9,48 +8,47 @@ function ToDoList() {
     { id: 2, task: "sleep" },
     { id: 3, task: "work" },
   ];
-  const [Todos,setTodos]=useState(todoList);
-  const [task,setTask]=useState("");
-  
-  const handleChange= (e) => {
-    setTask(e.target.value)
-  }
-  const handleAdd= () => {
-      const newTodos = Todos.concat({task, id: uuidv4()});
-      setTodos(newTodos);
-      setTask('');
-}
+  const [todos, setTodos] = useState(todoList);
+  const [task, setTask] = useState("");
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+  const handleAdd = (e) => {
+    e.preventDefault();
+    // const newTodos = Todos.concat({ task, id: uuidv4() });
+    // setTodos(newTodos);
+    console.log(task);
+    const newTask = { id: uuidv4(), task: task };
+    setTodos([...todos, newTask]);
+    console.log(todos);
+
+    setTask("");
+  };
   return (
-      <div className="container">
-          <h1>My ToDo List</h1>
-          <div className="col-12 m-4">
-              <form className="row">
-                  <input
-                  className="mr-4"
-                  placeholder="add a new task"
-                  value={task}
-                  onChange={handleChange}
-                  />
-                  <button onClick={handleAdd}>
-                    Add
-                  </button>
-              </form>
-          </div>
-         <div>
-         {
-              todoList.map((td)=>{
-                  return(
-                      <div className="col-12" key={td.id}>
-                          <SingleToDo data={td}/>
-                          <hr/>
-                      </div>
-
-                  );
-              })
-          }
-         </div>
+    <div className="container">
+      <h1>My ToDo List</h1>
+      <div className="col-12 m-4">
+        <form className="row">
+          <input
+            className="mr-4"
+            placeholder="add a new task"
+            value={task}
+            onChange={handleChange}
+          />
+          <button onClick={handleAdd}>Add</button>
+        </form>
       </div>
-
+      <div>
+        {todos.map((td) => {
+          return (
+            <div className="col-12" key={td.id}>
+              <SingleToDo data={td} />
+              <hr />
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
