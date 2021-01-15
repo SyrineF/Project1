f0ad7027c2e4d0dd5382202f5010e5b2961a4516
SyrineF <syrineferjani.isam@gmail.com>
2021-01-15 11:23:38 +0100

    components added

---
+++ src/components/ToDo/SingleToDo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SingleToDo() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SingleToDo
---
+++ src/components/ToDo/ToDoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SingleToDo from "./SingleToDo"
+
+function ToDoList() {
+    return (
+        <div>
+            {
+                
+
+            }
+        </div>
+    )
+}
+
+export default ToDoList
